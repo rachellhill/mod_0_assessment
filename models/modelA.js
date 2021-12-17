090a5fb041fe751e5f070673067efda3bdfea75c
Rachel Hill <hillrachel187@gmail.com>
2021-12-16 20:32:03 -0600

    Update modelA file

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaurModel = {
+  name: 'T. rex',
+  species: 'Tyrannasaurus rex',
+  yearsDeceased: 100000000,
+  avgLifeLength: 10,
+  isCarnivore: true,
+  isViolent: true,
+  mapArea: ['Australia', 'North America', 'South America'],
+  otherSpecies: ['Pterodactyl', 'Brachiosaurus', 'Raptor']
+}
