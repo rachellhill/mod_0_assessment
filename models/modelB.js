324d967b3a44deae77b60cd291ac1848b6804dab
Rachel Hill <hillrachel187@gmail.com>
2021-12-16 20:40:22 -0600

    Update modelB file

---
+++ models/modelB.js
@@ -0,0 +1,13 @@
+var developerFrontEnd = {
+  name: 'Rachel',
+  skill: 'Javascript',
+  yearsOfExperience: 7,
+  jobsHeld: 5,
+  hasLeadershipExperience: true,
+  isCurrentlyWorking: true,
+  priorRoles: ['Jr. FE Developer', 'Full Stack Developer', 'UX Designer'],
+  priorCompanies: ['Slack', 'Google', 'FaceBook']
+
+}
+
+console.log(developerFrontEnd);
