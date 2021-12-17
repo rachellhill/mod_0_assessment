e2762d56355e2d13092b9ff54c29688eba713fce
Rachel Hill <hillrachel187@gmail.com>
2021-12-16 20:18:48 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,21 @@
+var movie = 'Spiderman';
+console.log(movie);
+
+var isEntertaining = true;
+
+movie = 'Bridesmaids';
+console.log(movie);
+
+var num4 = 17;
+var num6 = 13;
+var num8 = num4 * num6;
+console.log(num8);
+
+var favoriteFoods = ['bagels', 'pizza', 'cookies'];
+console.log(favoriteFoods);
+
+favoriteFoods.shift();
+console.log(favoriteFoods);
+
+
+console.log(`${movie} ${movie.length}`);
